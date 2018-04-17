b194d036e46a44d785218ace9ffee9159c142fb9
Sam Davey <samkdavey@gmail.com>
2018-04-17 19:48:53 +0100

    Update

--- mainPage.js
+++ mainPage.js
@@ -1,29 +1,29 @@
 $('.homelink').on('click', function() {
     var body = $("html, body");
-    body.stop().animate({scrollTop:$('#logo').offset().top}, '500');
+    body.stop().animate({scrollTop:$('#nameTitle').offset().top-200}, '500');
 })
 
 $('.aboutlink').on('click', function() {
     var body = $("html, body");
-    body.stop().animate({scrollTop:$('#about').offset().top}, '500');
+    body.stop().animate({scrollTop:$('#about').offset().top-100}, '500');
 })
 
 $('.skillslink').on('click', function() {
     var body = $("html, body");
-    body.stop().animate({scrollTop:$('#skills').offset().top}, '500');
+    body.stop().animate({scrollTop:$('#skills').offset().top-100}, '500');
 })
 
 $('.projectlink').on('click', function() {
     var body = $("html, body");
-    body.stop().animate({scrollTop:$('#projects').offset().top}, '500');
+    body.stop().animate({scrollTop:$('#projects').offset().top-100}, '500');
 })
 
 $('.cvlink').on('click', function() {
     var body = $("html, body");
-    body.stop().animate({scrollTop:$('#cv').offset().top}, '500');
+    body.stop().animate({scrollTop:$('#cv').offset().top-100}, '500');
 })
 
 $('.contactlink').on('click', function() {
     var body = $("html, body");
-    body.stop().animate({scrollTop:$('#contact').offset().top}, '500');
+    body.stop().animate({scrollTop:$('#contact').offset().top-100}, '500');
 })
