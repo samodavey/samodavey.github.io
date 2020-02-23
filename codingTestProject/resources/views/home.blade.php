d5d3493a2750f08ba30be75f2267952cbc5a7ced
Sam Davey <samkdavey@gmail.com>
2020-02-23 17:02:57 +0700

    Calling Title and Author from mySQL data

--- codingTestProject/resources/views/home.blade.php
+++ codingTestProject/resources/views/home.blade.php
@@ -39,39 +39,49 @@
 
               <div class="card-body">
 
-            <div style="width: 600px; margin: 0 auto;">
+            <div>
               <table class="table">
               <thead>
                 <tr>
-
-                  <th scope="col">First</th>
-                  <th scope="col">Last</th>
-                  <th scope="col">Handle</th>
+                  <th scope="col">Title</th>
+                  <th scope="col">Author</th>
+                  <th scope="col">Edit / Delete</th>
                 </tr>
               </thead>
               <tbody>
+                <?php
+                $connect = new mysqli("localhost", "root", "", "Library");
+                if($connect != null){
+                 // echo "connected";
+                }else {
+                 // echo "failed to connect ";
+                }
+                $sqlQuery = "SELECT * FROM Books";
+                $qry = $connect->query($sqlQuery);
+
+                while($row = mysqli_fetch_array($qry)):;?>
                 <tr>
-                  <td>Mark</td>
-                  <td>Otto</td>
-                  <td>@mdo</td>
+                  <td><?php echo $row[1];?></td>
+                  <td><?php echo $row[2];?></td>
+                  <td>
+                    
+                  </td>
                 </tr>
-                <tr>
-                  <td>Jacob</td>
-                  <td>Thornton</td>
-                  <td>@fat</td>
-                </tr>
-                <tr>
-                  <td>Larry</td>
-                  <td>the Bird</td>
-                  <td>@twitter</td>
-                </tr>
+              <?php endwhile;?>
               </tbody>
             </table>
+            <script>
+              $(document).ready(function()){
+                $('.delete_book').on('submit', function()){
+                  alert('Clicked!');
+                });
+              });
+            </script>
             </div>
               </div>
             </div>
-
         </div>
     </div>
 </div>
+
 @endsection
