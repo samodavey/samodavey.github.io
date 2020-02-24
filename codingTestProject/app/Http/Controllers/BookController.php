8d2019660950aeb97e374dfb00e15698032d51e6
Sam Davey <samkdavey@gmail.com>
2020-02-24 18:07:00 +0700

    Add books to mySQL table via Laravel Implemented
    - Includes validation checking (Cannot be null)

---
+++ codingTestProject/app/Http/Controllers/BookController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Book;
+
+class BookController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $books = Book::all()->toArray();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        return view('home');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+          'title' => 'required',
+          'author' => 'required'
+        ]);
+
+        $book = new Book([
+          'title' => $request->get('title'),
+          'author' => $request->get('author'),
+        ]);
+        $book->save();
+        return redirect()->route('home')->with('success','Data Added');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- codingTestProject/resources/views/home.blade.php
+++ codingTestProject/resources/views/home.blade.php
@@ -18,19 +18,34 @@
                 </div>
 
             </div> -->
-
+            @if(count($errors) > 0)
+              <div class="alert alert-danger">
+                <ul>
+                  @foreach($errors->all() as $error)
+                  <li>{{$error}}</li>
+                  @endforeach
+                </ul>
+              </div>
+            @endif
+            @if(\Session::has('success'))
+              <div class="alert alert-success">
+                <p>{{\Session::get('success')}}</p>
+              </div>
+            @endif
             <div class="card">
               <div class="card-header">
                 <div class="formContainer">
-                  <form>
+                  <form method="post" action="{{url('book')}}">
+                    {{csrf_field()}}
                     <div class="form-group formField">
                       <label for="inputTitle">Title</label>
-                      <input type="title" class="form-control" id="inputTitle" aria-describedby="emailHelp">
+                      <input type="text" class="form-control" name="title">
                       <small class="form-text text-muted">Please add the title of the book</small>
                     </div>
                     <div class="form-group formField">
                       <label for="inputAuthor">Author</label>
-                      <input type="author" class="form-control" id="inputAuthor">
+                      <input type="text" class="form-control" name="author">
+                      <small class="form-text text-muted">Please add the author of the book</small>
                     </div>
                     <button type="submit" class="btn btn-primary">Add</button>
                 </form>
